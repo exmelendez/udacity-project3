f34c0e3350dde890d9bfd7ced0d91ade8898b035
exmelendez <exmelendez@gmail.com>
2022-07-18 22:33:21 -0400

    Feat: added express, gitignore plus designed for successful fetch from OpenWeather

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+  thing: "some things here",
+  name: 'Eddie',
+  age: 37,
+};
 
 // Require Express to run server and routes
 const express = require('express');
@@ -7,8 +11,23 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
+app.get('/', (req, res) => {
+  console.log('console log', projectData);
+  res.status(200).send(projectData);
+});
+
+app.get('/all', (req, res) => {
+  console.log('all route');
+});
+
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -23,8 +42,8 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Setup Server
-const port = 3000;
-const server = app.listen(port, listening);
+const port = process.env.PORT || 4000;
+app.listen(port, listening);
 
 function listening() {
   console.log(`runnin on localhost: ${port}`);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,93 @@
 /* Global Variables */
+const generateBtn = document.getElementById('generate');
+const zipcodeInput = document.getElementById('zip');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+/*
+fetch('/')
+  .then(response => response.json())
+  .then(data => console.log(data));
+*/
+const getLatLon = async (baseUrl, zip, apiKey) => {
+  const geoUrl = `geo/1.0/zip?zip=${zip},US`;
+
+  const res = await fetch(`${baseUrl}${geoUrl}${apiKey}`)
+  try {
+    const data = await res.json();
+    return await data;
+  } catch (err) {
+    console.log('error:', err);
+  }
+};
+
+const getWeather = async (baseUrl, lat, lon, apiKey) => {
+  const weatherUrl = `data/2.5/weather?lat=${lat}&lon=${lon}`;
+
+  const res = await fetch(`${baseUrl}${weatherUrl}${apiKey}&units=imperial`)
+  try {
+    const data = await res.json();
+    return await data;
+  } catch (err) {
+    console.log('error:', err);
+  }
+};
+
+const submitEntry = async zip => {
+  const baseUrl = 'https://api.openweathermap.org/';
+  const apiKey = '&appid=d5b6158d16fade3dcd73273e5729301b';
+
+  const geoData = await getLatLon(baseUrl, zip, apiKey);
+  const weatherData = await getWeather(baseUrl, geoData.lat, geoData.lon, apiKey);
+  console.log('geoData:', geoData);
+  console.log('weatherData:', weatherData);
+
+  /*
+  const res = await fetch(`${baseUrl}${geoUrl}${apiKey}`)
+  try {
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log('error:', err);
+  }
+  */
+
+  /*
+  .then(response => response.json())
+  .then(data => console.log(data));
+  */
+};
+
+generateBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  submitEntry(zipcodeInput.value);
+  zipcodeInput.value = '';
+});
+
+/*
+const retrieveData = async (url='') =>{ 
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  return await allData;
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+retrieveData('http://127.0.0.1/:4000');
+*/
+
+/*
+FETCH INFO TO USE ZIP TO OBTAIN LAT/LON
+https://api.openweathermap.org/geo/1.0/zip?zip=10463,US&appid=d5b6158d16fade3dcd73273e5729301b
+
+
+FETCH INFO FOR WEATHER
+https://api.openweathermap.org/data/2.5/weather?lat=40.8798&lon=-73.9067&appid=d5b6158d16fade3dcd73273e5729301b
+*/
